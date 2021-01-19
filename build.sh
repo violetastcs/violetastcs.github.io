50be80b5317c4884bf4a37d493a76e46db338963
Violet <violet.hates.glowies@protonmail.com>
2021-01-19 20:31:44 +0000

    updated build script to remove reference to OMCI

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@
 
 # build.sh
 # by: Violetas (The Coom Slayer) <violet.hates.glowies@protonmail.com>, January 2021
-# description: simple static site generator for OMCI
+# description: simple static site generator written in POSIX shell script.
 
-# This file is part of the OMCI website.
+# This file is part of Violetas' website.
 #
-# The OMCI website is free software: you can redistribute it and/or modify
+# Violetas' website is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# The OMCI website is distributed in the hope that it will be useful,
+# Violetas' website is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with the OMCI website. If not, see <https://www.gnu.org/licenses/>.
+# along with Violetas' website. If not, see <https://www.gnu.org/licenses/>.
 
 mkdir -p build/static build/posts
 
